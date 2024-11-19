2745890581766758bbc2d6a7d47e49840eed4b8d
charlie <charloako1@gmail.com>
2024-11-19 21:41:26 +0300

    image rendering

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 import { withNextVideo } from "next-video/process";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['plus.unsplash.com'], // Add any external domains from which you are loading images
+  },
+};
 
 export default withNextVideo(nextConfig);
