c823a2bbd6b519e335a51c10c8f9f1cc7c03f9ce
charlie <charloako1@gmail.com>
2024-12-29 13:51:05 +0300

    services> webdevelopment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,28 @@
-import { withNextVideo } from "next-video/process";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['plus.unsplash.com', 'media.istockphoto.com','images.unsplash.com'], // Add any external domains from which you are loading images
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.istockphoto.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '**',
+      },
+    ],
   },
 };
 
-export default withNextVideo(nextConfig);
+export default nextConfig
+
+
+
