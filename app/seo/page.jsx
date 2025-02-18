66f8a27d4299b3da29a7b46c94b91ff6c3b940fd
charlie <charloako1@gmail.com>
2025-02-18 14:36:19 +0300

    site map added

--- app/seo/page.jsx
+++ app/seo/page.jsx
@@ -15,9 +15,8 @@ const cardContent = {
   description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, hic ipsum!",
 };
 
-const SocialMediaMarketingProcess  = [
+const SocialMediaMarketingProcess = [
   {
-   
     url: "/imgs/abstract/1.jpg",
     title: "Planning",
     id: 1,
@@ -28,7 +27,7 @@ const SocialMediaMarketingProcess  = [
     title: "Create",
     id: 2,
     step: " Design visually appealing and engaging content (images, videos, captions).Tailor content to each platform and use trending formats (Reels, Stories, etc.).Focus on consistency and aligning content with your brand voice.",
-  },  
+  },
   {
     url: "/imgs/abstract/3.jpg",
     title: "Engage",
@@ -41,7 +40,6 @@ const SocialMediaMarketingProcess  = [
     id: 4,
     step: " Track key metrics (reach, engagement, conversions) using analytics tools.Assess what works and refine your strategy accordingly.Test new content, posting times, and ad strategies for continuous improvement",
   },
-  
 ];
 
 const SeoItems = [
@@ -67,10 +65,7 @@ const SeoItems = [
   },
 ];
 
-
-
-
-const CardBody = ({ className="" }) => (
+const CardBody = ({ className = "" }) => (
   <div className={`px-2 text-gray-100 sm:px-4 py-0 sm:pb-3 text-left ${className}`}>
     <h3 className="text-lg font-bold tracking-tighter mt-3 mb-1">
       {cardContent.title}
@@ -80,11 +75,10 @@ const CardBody = ({ className="" }) => (
 );
 
 const list = [
-  {text:"Increase site Authority", icon:<FaCheck style={{color:'green'}}/>},
-  {text:"Attract Organic Leads", icon:<FaCheck style={{color:'green'}}/>},
-  {text:"Increase Organic Leads", icon:<FaCheck style={{color:'green'}}/>},
-]
-
+  { text: "Increase site Authority", icon: <FaCheck style={{ color: 'green' }} /> },
+  { text: "Attract Organic Leads", icon: <FaCheck style={{ color: 'green' }} /> },
+  { text: "Increase Organic Leads", icon: <FaCheck style={{ color: 'green' }} /> },
+];
 
 const CardWithImageV1 = ({ children }) => (
   <div className="relative rounded-2xl aspect-w-4 aspect-h-3 overflow-hidden group">
@@ -93,10 +87,9 @@ const CardWithImageV1 = ({ children }) => (
       alt="Responsive web design illustration"
       placeholder="blur"
       blurDataURL="https://media.istockphoto.com/id/1371339413/photo/co-working-team-meeting-concept-businessman-using-smart-phone-and-digital-tablet-and-laptop.jpg?s=612x612&w=0&k=20&c=ysEsVw3q2axYt3oVZAuQjtHRlN3lY-U_e0ikK5yKIXQ="
-      height={450} 
-      width={600}   
+      height={450}
+      width={600}
       className=''
-      
     />
     {/* overlay */}
     {/* <div className="absolute inset-0 bg-gradient-to-t from-stone-900/90 via-stone-900/60 to-stone-900/5 backdrop-blur-[2px] transition-all duration-300"></div>
@@ -105,68 +98,66 @@ const CardWithImageV1 = ({ children }) => (
 );
 
 function SocialMediaMarketing() {
-  const {setIsLoading} =useLoading()
-   useEffect(() => {
-      // Set loading state when the component mounts
-      setIsLoading(true);
-  
-      // Mimic actual loading tasks
-      const timer = setTimeout(() => {
-        setIsLoading(false);
-      }, 2000); // Replace with actual loading time if needed
-  
-      return () => clearTimeout(timer); // Cleanup timer
-    }, [setIsLoading]);
+  const { setIsLoading } = useLoading();
+  useEffect(() => {
+    // Set loading state when the component mounts
+    setIsLoading(true);
+
+    // Mimic actual loading tasks
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Replace with actual loading time if needed
+
+    return () => clearTimeout(timer); // Cleanup timer
+  }, [setIsLoading]);
+
   return (
     <div className="min-h-full mt-36">
       <div className="grid md:grid-cols-2 gap-4 mt-10 px-9 mb-10">
-        <div className=''>
-        <h2 className="text-4xl font-bold heading">Expert SEO Services for Business Growth</h2>
+        <div>
+          <h2 className="text-4xl font-bold heading">Expert SEO Services for Business Growth</h2>
           <p className="mt-3 primary">
-            Boost your online presence with Digital Cheuxes Seo  Agency, a top-rated SEO company. We specialize in increasing traffic, improving rankings, and driving conversions.
+            Boost your online presence with Digital Cheuxes Seo Agency, a top-rated SEO company. We specialize in increasing traffic, improving rankings, and driving conversions.
           </p>
 
           <ul>
             {list.map((item, index) => (
               <li key={index} className="flex text-2xl gap-2">
                 <span className="relative mt-2 font-bold">{item.icon}</span>
-                <span className='primary'>{item.text} </span> 
+                <span className="primary">{item.text}</span>
               </li>
             ))}
           </ul>
-
-        </div>  
+        </div>
         <div>
           <CardWithImageV1>
-              <CardBody className="absolute px-4 pb-4 inset-0 flex flex-col justify-end size-full" />
-            </CardWithImageV1>
+            <CardBody className="absolute px-4 pb-4 inset-0 flex flex-col justify-end size-full" />
+          </CardWithImageV1>
         </div>
-        </div>
-        <div className='grid md:grid-cols-2 theme'>
-          <div className=''>
-            <h2 className='font-bold text-3xl capitalize ml-3'>SEO Services</h2>
-            <ExpandableCard>
+      </div>
+      <div className="grid md:grid-cols-2 theme">
+        <div>
+          <h2 className="font-bold text-3xl capitalize ml-3">SEO Services</h2>
+          <ExpandableCard>
             <p className="primary ml-5">
-              At <strong>Digital Cheuxes Agency</strong>, our expert SEO team works with you every step of the way to boost your website's visibility and rankings. We take a <strong>data-driven approach</strong> to SEO, ensuring that your business achieves <strong>higher search engine rankings, increased organic traffic, and better conversion rates</strong>. Our strategy includes <strong>keyword research, on-page and off-page optimization, technical SEO, and content marketing</strong>—all tailored to align perfectly with your brand’s goals. Whether you need <strong>local SEO, e-commerce SEO, or enterprise-level optimization</strong>, we’ve got you covered. Let’s create an SEO strategy that drives real results!
+              At <strong>Digital Cheuxes Agency</strong>, our expert SEO team works with you every step of the way to boost your website&apos;s visibility and rankings. We take a <strong>data-driven approach</strong> to SEO, ensuring that your business achieves <strong>higher search engine rankings, increased organic traffic, and better conversion rates</strong>. Our strategy includes <strong>keyword research, on-page and off-page optimization, technical SEO, and content marketing</strong>—all tailored to align perfectly with your brand&apos;s goals. Whether you need <strong>local SEO, e-commerce SEO, or enterprise-level optimization</strong>, we&apos;ve got you covered. Let&apos;s create an SEO strategy that drives real results!
             </p>
-
-            </ExpandableCard>
-            <ListHover items={SeoItems}/>
-          </div>
-          <div className='grid place-content-center'>
-            <Form/>
-          </div>
+          </ExpandableCard>
+          <ListHover items={SeoItems} />
         </div>
-        {/* <Slider 
-        
-        title="Social Media Marketing Best Practices"
-        processSteps={SocialMediaMarketingProcess}
-        FooterText="Social Media Marketing Best Practices"
+        <div className="grid place-content-center">
+          <Form />
+        </div>
+      </div>
+      {/* <Slider 
+          title="Social Media Marketing Best Practices"
+          processSteps={SocialMediaMarketingProcess}
+          FooterText="Social Media Marketing Best Practices"
         /> */}
-        <Faqs
-        title='FAQS'
+      <Faqs
+        title="FAQS"
         faqs={SeoFaqs}
-        />
+      />
     </div>
   );
 }
@@ -188,10 +179,10 @@ const SeoFaqs = [
   },
   {
     text: "Will my SEO strategy align with my brand identity?",
-    description: "Yes, we ensure that all SEO efforts, including content creation and optimization, align with your brand identity. We maintain consistency in tone, style, and messaging to reflect your brand's values and appeal to your audience."
+    description: "Yes, we ensure that all SEO efforts, including content creation and optimization, align with your brand identity. We maintain consistency in tone, style, and messaging to reflect your brand&apos;s values and appeal to your audience."
   },
   {
     text: "What types of reports will I receive for my SEO campaigns?",
-    description: "You’ll receive comprehensive reports that include metrics such as keyword rankings, organic traffic, backlink profiles, and conversion rates. These reports provide actionable insights to track progress and refine your SEO strategy."
+    description: "You&apos;ll receive comprehensive reports that include metrics such as keyword rankings, organic traffic, backlink profiles, and conversion rates. These reports provide actionable insights to track progress and refine your SEO strategy."
   }
 ];
