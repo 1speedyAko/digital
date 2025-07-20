9dad5d7fa43ad341de2345688eaf1dc20bf23081
Charlie <charloako1@gmail.com>
2025-07-20 18:20:32 +0300

    navbar mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
-        <TempoInit />
+        {/* <TempoInit /> */}
       </body>
     </html>
   );
